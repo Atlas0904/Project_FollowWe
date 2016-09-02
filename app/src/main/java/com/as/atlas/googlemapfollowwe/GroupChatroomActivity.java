2a56a3859ae0b3f8ff9940a592e6e187646db65f
atlas0904 <u920243@gmail.com>
2016-09-02 16:50:37 +0800

    Share action bar for different activity

--- app/src/main/java/com/as/atlas/googlemapfollowwe/GroupChatroomActivity.java
+++ app/src/main/java/com/as/atlas/googlemapfollowwe/GroupChatroomActivity.java
@@ -2,6 +2,7 @@ package com.as.atlas.googlemapfollowwe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -48,6 +49,9 @@ public class GroupChatroomActivity extends AppCompatActivity
         listView.setAdapter(chatroomAdapter);
 
         buttonSend.setOnClickListener(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         setTitle("Group chat Room: " + currentUserInfo.roomNo);
     }
 
